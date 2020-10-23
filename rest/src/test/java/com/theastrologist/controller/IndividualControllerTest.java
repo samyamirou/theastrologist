515ac03d6dbc4a50a6cd43d7aa7a63070ae63f83
Samy Amirou <samyamirou@gmail.com>
2020-10-23 18:20:20 +0200

    feat(primary-directions): in progress - ajout de tests pour les individuals avec un natalTheme. Next step : tester le post

--- rest/src/test/java/com/theastrologist/controller/IndividualControllerTest.java
+++ rest/src/test/java/com/theastrologist/controller/IndividualControllerTest.java
@@ -2,10 +2,15 @@ package com.theastrologist.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theastrologist.data.service.UserDataService;
+import com.theastrologist.domain.Degree;
+import com.theastrologist.domain.SkyPosition;
 import com.theastrologist.domain.individual.Individual;
 import com.theastrologist.domain.user.User;
 import com.theastrologist.exception.TooManyResultsException;
 import com.theastrologist.service.IndividualService;
+import com.theastrologist.service.ThemeService;
+import com.theastrologist.util.CalcUtil;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,7 +30,9 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.IsInstanceOf.any;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -33,6 +40,15 @@ import static org.junit.Assert.*;
 @AutoConfigureMockMvc
 public class IndividualControllerTest {
 
+	private final DateTime TEST_NATAL_DATE = new DateTime(1985, 1, 4, 11, 20, CalcUtil.DATE_TIME_ZONE);
+	private final DateTime TEST_REV_DATE = new DateTime(2016, 3, 12, 05, 36, CalcUtil.DATE_TIME_ZONE);
+	private final Degree NATAL_LATITUDE = new Degree(48, 39);
+	private final Degree NATAL_LONGITUDE = new Degree(2, 25);
+
+	private SkyPosition testSkyPosition;
+
+	@Autowired
+	private ThemeService themeService;
 	@Autowired
 	private MockMvc mockMvc;
 
@@ -48,6 +64,7 @@ public class IndividualControllerTest {
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
+		testSkyPosition = themeService.getSkyPosition(TEST_NATAL_DATE, NATAL_LATITUDE, NATAL_LONGITUDE);
 	}
 
 	@Test
@@ -70,6 +87,29 @@ public class IndividualControllerTest {
 
 		assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
 		assertThat(body, hasJsonPath("name", equalTo(individualName)));
+	}
+
+	@Test
+	public void getIndividualWithPositions() throws Exception, TooManyResultsException {
+		String username = "toto";
+		String individualName = "titi";
+		User user = new User(username);
+		Individual individual = new Individual(individualName);
+		individual.setNatalTheme(testSkyPosition);
+
+		Mockito.when(userDataService.getUserByName(username)).thenReturn(user);
+		Mockito.when(individualService.findIndividualByName(user, individualName)).thenReturn(individual);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/user/{username}/individual/{individualName}", username, individualName)
+				.accept(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String body = result.getResponse().getContentAsString();
+		MockHttpServletResponse response = result.getResponse();
+
+		assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
+		assertThat(body, hasJsonPath("natalTheme.date", equalTo("1985-01-04T11:20:00+01:00")));
 	}
 
 	@Test
